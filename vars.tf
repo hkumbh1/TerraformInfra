bb793a85ce4c7973e9c80370a75415a5f6560bf3
Aashutosh Desale <50070351+fallenmaverick@users.noreply.github.com>
2022-12-09 10:36:04 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,30 @@
+
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "ap-south-1"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "jenkins-slave"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "launch-wizard-2"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "web-instance"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0533f2ba8a1995cf9"
+}
